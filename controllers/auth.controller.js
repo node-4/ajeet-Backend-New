72c6a4681bf064bb5074cae7d33cb2dd8a1fe863
rubiyadav <rubiflyeis@gmail.com>
2023-06-03 19:14:45 +0530

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,36 +1,34 @@
 const expressAsyncHandler = require("express-async-handler");
 const User = require("../models/auth.model");
-const wallet = require('../models/wallet')
+const wallet = require("../models/wallet");
 const { emailValidtaion } = require("../Helper/validation");
 const jwt = require("jsonwebtoken");
-const JWTkey = "node3"
-var newOTP = require('otp-generators');
-
-
+const JWTkey = "node3";
+var newOTP = require("otp-generators");
 
 module.exports.isAuthenticated2 = (req, res, next) => {
-  if (req.headers.authorization) {
-    console.log("entered authorization");
-    const token = req.headers.authorization.split(" ")[1];
-    const user = jwt.verify(token, JWTkey);
-    req.user = user.id;
-    next();
-  } else {
-    return res.status(401).json({ message: "Authorization required" });
-  }
+    if (req.headers.authorization) {
+        console.log("entered authorization");
+        const token = req.headers.authorization.split(" ")[1];
+        const user = jwt.verify(token, JWTkey);
+        req.user = user.id;
+        next();
+    } else {
+        return res.status(401).json({ message: "Authorization required" });
+    }
 };
 module.exports.userMiddleware = (req, res, next) => {
-  console.log(req.user);
-  User.findById(req.user).exec((err, user) => {
-    if (user) {
-      next();
-    }
-    if (err) {
-      return res.status(400).json({
-        message: "User access denied",
-      });
-    }
-  });
+    console.log(req.user);
+    User.findById(req.user).exec((err, user) => {
+        if (user) {
+            next();
+        }
+        if (err) {
+            return res.status(400).json({
+                message: "User access denied",
+            });
+        }
+    });
 };
 
 // const sendOTP = async (phoneNumber, req, res) => {
@@ -77,151 +75,159 @@ module.exports.userMiddleware = (req, res, next) => {
 //     });
 // };
 
+module.exports.sendOtp = async (req, res) => {
+    try {
+        const name = req.body.name;
+        const phoneNumber = req.body.phoneNumber;
+        if (!(phoneNumber && name)) {
+            return res.status(400).json({
+                message: "Name and PhoneNumber is Required ",
+            });
+        }
+        const data = await User.findOne({ phoneNumber: phoneNumber });
+        if (data) {
+            return res.status(200).json({
+                otp: data.otp,
+                isRegistered: data.isRegisered,
+            });
+        } else {
+            const otp = newOTP.generate(4, {
+                alphabets: false,
+                upperCase: false,
+                specialChar: false,
+            });
+            const data = await User.create({
+                phoneNumber: phoneNumber,
+                otp: otp,
+                name: name,
+            });
+            res.status(200).json({
+                otp: data.otp,
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(501).json({
+            message: err.message,
+        });
+    }
+};
 
-module.exports.sendOtp = async(req,res) => {
-  try{
-  const name = req.body.name;
-  const phoneNumber = req.body.phoneNumber;
-  if(!(phoneNumber && name)){
-    return res.status(400).json({
-      message: "Name and PhoneNumber is Required "
-    })
-  }
-    const data = await User.findOne({phoneNumber:phoneNumber})
-    if(data){
-    return res.status(200).json({
-      otp: data.otp,
-      isRegistered: data.isRegisered
-    })
-  }else{
-  const otp = newOTP.generate(4, { alphabets: false, upperCase: false, specialChar: false });
-  const data = await User.create({phoneNumber: phoneNumber, otp: otp, name: name});
-  res.status(200).json({
-    otp: data.otp
-  })
-}
-  }catch(err){
-    console.log(err)
-    res.status(501).json({
-      message: err.message
-    })
-  }
-}
+module.exports.veriifyOTP = async (req, res) => {
+    try {
+        const otp = req.body.otp;
+        const data = await User.findOne({ otp: otp });
+        const Wd = await wallet.findOne({ user: data._id });
+        if (!data) {
+            return res.status(400).json({
+                message: "Otp wrong retry",
+            });
+        } else {
+            if (!Wd) {
+                const w = await wallet.create({ user: data._id });
+            }
+            jwt.sign(
+                { _id: data._id },
+                JWTkey,
+                { expiresIn: "24h" },
+                (err, token) => {
+                    if (err) res.status(400).send("Invalid Credentials");
+                    res.send({
+                        token,
+                        Id: data._id,
+                        isRegistered: data.isRegisered,
+                        role: data.role,
+                        msg: "verified! 👍",
+                    });
+                }
+            );
 
-module.exports.veriifyOTP = async(req,res) => {
-  try{
-  const otp = req.body.otp;
-  const data =await User.findOne({otp: otp});
-  const Wd = await wallet.findOne({user: data._id})
-  if(!data){
-    return res.status(400).json({
-      message: "Otp wrong retry"
-    })
-   
-  }else{
-     if(!Wd){
-      const w = await wallet.create({user: data._id})
-     }
-     jwt.sign(
-          { _id: data._id },
-          JWTkey,
-          { expiresIn: "24h" },
-          (err, token) => {
-            if (err) res.status(400).send("Invalid Credentials");
-            res.send({ token, Id: data._id, isRegistered: data.isRegisered , role: data.role, msg: "verified! 👍" });
-          }
-        );
-
-        console.log("verified! 👍");
-      }
-  }catch(err){
-    res.status(501).json({
-      message: err.message
-    })
-  }
-}
+            console.log("verified! 👍");
+        }
+    } catch (err) {
+        res.status(501).json({
+            message: err.message,
+        });
+    }
+};
 
 module.exports.login = expressAsyncHandler(async (req, res) => {
-  const name = req.body.name
-  const phoneNumber = req.body.phoneNumber
-console.log(phoneNumber)
-  try {
-    if (!(phoneNumber && name)) {
-    return  res.status(401).send({ msg: "Required filleds" });
-    }
-
-    var user = await User.findOne({ phoneNumber });
-
-    if (user) {
-      jwt.sign(
-        { user_id: user._id },
-        JWTkey,
-        { expiresIn: "24h" },
-        (err, token) => {
-          console.log(token)
-       //   if (err) res.status(400).send("Invalid Credentials");
-          res.send({ token, msg : "already logged in user" });
+    const name = req.body.name;
+    const phoneNumber = req.body.phoneNumber;
+    console.log(phoneNumber);
+    try {
+        if (!(phoneNumber && name)) {
+            return res.status(401).send({ msg: "Required filleds" });
         }
-      );
+
+        var user = await User.findOne({ phoneNumber });
+
+        if (user) {
+            jwt.sign(
+                { user_id: user._id },
+                JWTkey,
+                { expiresIn: "24h" },
+                (err, token) => {
+                    console.log(token);
+                    //   if (err) res.status(400).send("Invalid Credentials");
+                    res.send({ token, msg: "already logged in user" });
+                }
+            );
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error, msg: "Error occured" });
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error, msg: "Error occured" });
-    console.log(error);
-  }
 });
 
-module.exports.role = expressAsyncHandler(async(req,res) => {
-   try{
-    if(!req.params.userId){
-      return res.status(400).json({
-        message: "UserId is Required "
-      })
-    }else{
-      await User.findByIdAndUpdate({_id: req.params.userId}, {
-        role: req.body.role
-      })
-      res.status(200).json({
-        message: "Updted "
-      }) 
+module.exports.role = expressAsyncHandler(async (req, res) => {
+    try {
+        if (!req.params.userId) {
+            return res.status(400).json({
+                message: "UserId is Required ",
+            });
+        } else {
+            await User.findByIdAndUpdate(
+                { _id: req.params.userId },
+                { role: req.body.role }
+            );
+            res.status(200).json({
+                message: "Updted ",
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        });
     }
-  
-   }catch(err){
-    res.status(500).json({
-      message: err.message
-    })
-   }
-})
+});
 
 module.exports.updateProfile = expressAsyncHandler(async (req, res) => {
-  const id = req.params.id;
-  try {
-    const { tradeName, phoneNumber, photo, location,  email, address } =
-      req.body;
-
-    if (!( email)) {
-     return res.status(501).send({ msg: "Required filleds" });
+    const id = req.params.id;
+    try {
+        const { tradeName, phoneNumber, photo, location, email, address } =req.body;
+        if (!email) {
+            return res.status(501).send({ msg: "Required filleds" });
+        }
+        const data1 = await User.findById({ _id: req.params.id });
+        if (data1) {
+            let obj = {
+                tradeName: tradeName || data1.tradeName,
+                phoneNumber: phoneNumber || data1.phoneNumber,
+                location: location || data1.location,
+                address: address || data1.address,
+                photo: photo || data1.photo,
+                email: email || data1.email,
+                isRegisered: true,
+            };
+            const data = await User.findByIdAndUpdate(id, {obj},{new:true});
+            return res.status(200).send({ msg: "update successfully" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error, msg: "Error occured" });
     }
-
-    const data = await User.findByIdAndUpdate(id, {
-      tradeName,
-      phoneNumber,
-      location,
-      address:[{
-        homeaddress: req.body.homeaddress,
-        city: req.body.city ,
-        pincode: req.body.pincode,
-        state: req.body.state,
-      }],
-      photo,
-      email,
-      isRegisered: true
-    });
-    return res.status(200).send({ msg: "update successfully",  });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error, msg: "Error occured" });
-  }
 });
 
 // module.exports.register = expressAsyncHandler(async (req, res) => {
@@ -269,65 +275,63 @@ module.exports.updateProfile = expressAsyncHandler(async (req, res) => {
 // // });
 
 module.exports.getUserDetails = expressAsyncHandler(async (req, res) => {
-  try {
-    const data = await User.find();
-    res.status(200).send({ msg: "user details get successfully", data });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error, msg: "Error occured" });
-  }
-})
+    try {
+        const data = await User.find();
+        res.status(200).send({ msg: "user details get successfully", data });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error, msg: "Error occured" });
+    }
+});
 
-module.exports.GetAllUsersCondition  = expressAsyncHandler(async (req, res) => {
-  try {
-  if(req.params.type === "Buyer" ){
-    const data = await User.find({type: req.params.type})
-     return res.status(200).json({
-      message: data 
-     })
-  }else if(req.params.type === "Supplier"){
-    const data = await User.find({type: "Supplier"});
-    return res.status(200).json({
-      data : data
-    })
-  }else if(req.params.type === "Transporter"){
-    const data = await User.find({type: "Transporter"});
-    return res.status(200).json({
-      data : data
-    })
-  }
-  }catch(err){
-     res.status(500).json({
-      message: err.message
-    })
-  }
-})
+module.exports.GetAllUsersCondition = expressAsyncHandler(async (req, res) => {
+    try {
+        if (req.params.type === "Buyer") {
+            const data = await User.find({ type: req.params.type });
+            return res.status(200).json({
+                message: data,
+            });
+        } else if (req.params.type === "Supplier") {
+            const data = await User.find({ type: "Supplier" });
+            return res.status(200).json({
+                data: data,
+            });
+        } else if (req.params.type === "Transporter") {
+            const data = await User.find({ type: "Transporter" });
+            return res.status(200).json({
+                data: data,
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        });
+    }
+});
 
+module.exports.GetDetailsByID = expressAsyncHandler(async (req, res) => {
+    try {
+        const data = await User.findById({ _id: req.params.id });
+        res.status(200).json({
+            details: data,
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        });
+    }
+});
 
-module.exports.GetDetailsByID = expressAsyncHandler(async(req,res) => {
-  try{
-    const data = await User.findById({_id: req.params.id})
-    res.status(200).json({
-      details: data
-    })
-  }catch(err){
-    res.status(500).json({
-      message: err.message
-    })
-  }
-})
-
-
-exports.DeleteUserById = async(req,res) => {
-  try{
-  await User.findByIdAndDelete({_id: req.params.id});
-  res.status(200).json({
-    message: "User is Deleted "
-  })
-  }catch(err){
-    console.log(err);
-    res.status(400).json({
-      message: err.message
-    })
-  }
-}
+exports.DeleteUserById = async (req, res) => {
+    try {
+        await User.findByIdAndDelete({ _id: req.params.id });
+        res.status(200).json({
+            message: "User is Deleted ",
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({
+            message: err.message,
+        });
+    }
+};
