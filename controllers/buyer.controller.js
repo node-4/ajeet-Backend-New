a74a19a00753ea8148f626bf107a813c7ff059f2
rubiyadav <rubiflyeis@gmail.com>
2023-06-06 15:01:57 +0530

    create an api of userChat, create an api of viewChat, create an api of chattingHistory, create an api of deleteChat, create an api of clearChat, create an api of deleteAllChat, create an api of addBook, create an api of getBookingbypreferenceid, create an api of getbookingbyid, create an api of getallBooking, create an api of getBookingbyuserid, create an api of getbookingbystatus, create an api of deleteBooking, create an api of editbooking, create an api of cancelappointment, create an api of completedappointment

--- controllers/buyer.controller.js
+++ controllers/buyer.controller.js
@@ -6,33 +6,33 @@ const payment = require('../models/paymentSummary');
 const User = require("../models/auth.model");
 var data;
 module.exports.searchBid = expressAsyncHandler(async (req, res) => {
-  const { product} = req.body;
+  const { product } = req.body;
   const regex = new RegExp(product, 'i');
-  const cropData = await Crop.findById({_id: product})
+  const cropData = await Crop.findById({ _id: product })
   if (!product) {
-  return   res.status(500).send("Product required");
+    return res.status(500).send("Product required");
   }
   const Data = await Buyer.aggregate([
-    {$match: {product: cropData.name}},
-     {$sort: {highestBid: -1}},
-     {$limit: 1}
-   ]);
-   
-  
+    { $match: { product: cropData.name } },
+    { $sort: { highestBid: -1 } },
+    { $limit: 1 }
+  ]);
+
+
   try {
     console.log(regex)
-    data = await createBid.find({ crop: product}).populate(['user_id', 'crop'])
- //   console.log(data);
- if(Data.length !== 0){
-  return  res.status(200).json({data: data})
- }
+    data = await createBid.find({ crop: product }).populate(['user_id', 'crop'])
+    //   console.log(data);
+    if (Data.length !== 0) {
+      return res.status(200).json({ data: data })
+    }
     else if (!data) {
-    return  res.status(400).status.send("Product is not available");
-    }else{
-      if(Data.length ===0 ){
-        return   res.status(200).json({data: data})
+      return res.status(400).status.send("Product is not available");
+    } else {
+      if (Data.length === 0) {
+        return res.status(200).json({ data: data })
       }
-    return   res.status(200).json({data: data, highestBid : `heightBid On Bid ${Data[0].highestBid}`})
+      return res.status(200).json({ data: data, highestBid: `heightBid On Bid ${Data[0].highestBid}` })
     }
 
   } catch (error) {
@@ -44,9 +44,9 @@ module.exports.searchBid = expressAsyncHandler(async (req, res) => {
 module.exports.allUsers = async (req, res) => {
   const keyword = req.query.search
     ? {
-        $or: [{ crop: { $regex: req.query.search, $options: "i" } }],
-       // $or: [{ location: { $regex: req.query.search, $options: "i" } }],
-      }
+      $or: [{ crop: { $regex: req.query.search, $options: "i" } }],
+      // $or: [{ location: { $regex: req.query.search, $options: "i" } }],
+    }
     : {};
 
   const users = await createBid.find({ keyword });
@@ -60,11 +60,11 @@ module.exports.makeBid = expressAsyncHandler(async (req, res) => {
   try {
     const data = await createBid.findById(req.params.id);
     console.log(data.count)
-    const UserData = await User.findById({_id: req.body.user})
-  const cropData = await  Crop.findById({_id: req.body.product})
-  console.log(cropData)
-    let count = data.count +1;
- 
+    const UserData = await User.findById({ _id: req.body.user })
+    const cropData = await Crop.findById({ _id: req.body.product })
+    console.log(cropData)
+    let count = data.count + 1;
+
     const user = new Buyer({
       user: req.body.user,
       product: cropData.name,
@@ -77,18 +77,18 @@ module.exports.makeBid = expressAsyncHandler(async (req, res) => {
     });
     await user.save();
     const DataDB = await Buyer.aggregate([
-      {$match: {product: cropData.name}},
-       {$sort: {highestBid: -1}},
-       {$limit: 1}
-     ]);
-     console.log(DataDB)
-     console.log( DataDB[0].highestBid);
-     if(data.count >= 0){
-        data.count  =  count ,
+      { $match: { product: cropData.name } },
+      { $sort: { highestBid: -1 } },
+      { $limit: 1 }
+    ]);
+    console.log(DataDB)
+    console.log(DataDB[0].highestBid);
+    if (data.count >= 0) {
+      data.count = count,
         data.status = "PositiveBid",
-        data.topBid =  DataDB[0].highestBid
-        await data.save()
-     }
+        data.topBid = DataDB[0].highestBid
+      await data.save()
+    }
     res.status(200).json({ UserData, msg: data });
   } catch (error) {
     console.log(error);
@@ -133,7 +133,7 @@ module.exports.buyerDecline = expressAsyncHandler(async (req, res) => {
 
 module.exports.modifyBid = expressAsyncHandler(async (req, res) => {
   const id = req.params.id;
-  const  highestBid = req.body.highestBid
+  const highestBid = req.body.highestBid
   try {
     const data = await Buyer.findByIdAndUpdate(id, { highestBid: highestBid });
 
@@ -144,25 +144,25 @@ module.exports.modifyBid = expressAsyncHandler(async (req, res) => {
   }
 });
 
-module.exports.createpaymentSummary = async(req,res) => {
-  try{
-    const data = await Buyer.findById({_id: req.body.id})
-    const paymentData =  {
+module.exports.createpaymentSummary = async (req, res) => {
+  try {
+    const data = await Buyer.findById({ _id: req.body.id })
+    const paymentData = {
       name: data.bidDetail.crop,
       lotId: data.bidDetail.lotId,
       grade: data.bidDetail.grade,
-      quantity: data.quantity, 
-      tax: req.body.tax, 
-      ServiceCharge: req.body.ServiceCharge, 
-      others: req.body.others, 
+      quantity: data.quantity,
+      tax: req.body.tax,
+      ServiceCharge: req.body.ServiceCharge,
+      others: req.body.others,
       total: quantity * data.bidDetail
     }
     const result = await payment.create(paymentData);
     res.status(200).json({
       message: "ok",
-      result : result 
+      result: result
     })
-  }catch(err){
+  } catch (err) {
     console.log(err);
     res.status(400).json({
       message: err.message
@@ -172,29 +172,29 @@ module.exports.createpaymentSummary = async(req,res) => {
 
 
 
-exports.getAllPayment = async(req,res) => {
-  try{
-  const result = await payment.find().populate(['supplierId', 'buyerId']);
-  res.status(200).json({
-    message: "ok", 
-    result: result 
-  })
-  }catch(err){
+exports.getAllPayment = async (req, res) => {
+  try {
+    const result = await payment.find().populate(['supplierId', 'buyerId']);
+    res.status(200).json({
+      message: "ok",
+      result: result
+    })
+  } catch (err) {
     console.log(err);
     res.status(400).json({
       message: err.message
     })
   }
 }
-exports.getAllPaymentBuyerId = async(req,res) => {
-  try{
+exports.getAllPaymentBuyerId = async (req, res) => {
+  try {
     console.log(req.params.id)
-  const result = await payment.find({buyerId: req.params.id}).populate(['supplierId', 'buyerId']);
-  res.status(200).json({
-    message: "ok", 
-    result: result 
-  })
-  }catch(err){
+    const result = await payment.find({ buyerId: req.params.id }).populate(['supplierId', 'buyerId']);
+    res.status(200).json({
+      message: "ok",
+      result: result
+    })
+  } catch (err) {
     console.log(err);
     res.status(400).json({
       message: err.message
@@ -202,14 +202,14 @@ exports.getAllPaymentBuyerId = async(req,res) => {
   }
 }
 
-exports.getAllPaymentSupplierId = async(req,res) => {
-  try{
-  const result = await payment.find({supplierId: req.params.id}).populate(['supplierId', 'buyerId']);
-  res.status(200).json({
-    message: "ok", 
-    result: result 
-  })
-  }catch(err){
+exports.getAllPaymentSupplierId = async (req, res) => {
+  try {
+    const result = await payment.find({ supplierId: req.params.id }).populate(['supplierId', 'buyerId']);
+    res.status(200).json({
+      message: "ok",
+      result: result
+    })
+  } catch (err) {
     console.log(err);
     res.status(400).json({
       message: err.message
@@ -219,14 +219,14 @@ exports.getAllPaymentSupplierId = async(req,res) => {
 
 
 
-module.exports.getAllPlaceBid = async(req,res) => {
-  try{
-  const data = await Buyer.find();
-  res.status(200).json({
-    message: "ok", 
-    result: data
-  })
-  }catch(err){
+module.exports.getAllPlaceBid = async (req, res) => {
+  try {
+    const data = await Buyer.find();
+    res.status(200).json({
+      message: "ok",
+      result: data
+    })
+  } catch (err) {
     res.status(400).json({
       message: err.message
     })
@@ -236,62 +236,62 @@ module.exports.getAllPlaceBid = async(req,res) => {
 module.exports.UpdateBidDetails = expressAsyncHandler(async (req, res) => {
   const id = req.params.id;
   try {
-    const data = await Buyer.findByIdAndUpdate({_id: req.params.id}, { 
+    const data = await Buyer.findByIdAndUpdate({ _id: req.params.id }, {
       quantity: req.body.quantity,
-     });
+    });
 
-    res.status(200).send({ msg: "Bid is Update ",});
+    res.status(200).send({ msg: "Bid is Update ", });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error, msg: "Error occured" });
   }
 });
-module.exports.getByUserId= async(req,res) => {
-  try{
-    const Data = await Buyer.aggregate([{$sort: {highestBid: -1}},{$limit: 1}]);
-    const data = await Buyer.find({user: req.params.id,status:"Onhold" })
+module.exports.getByUserId = async (req, res) => {
+  try {
+    const Data = await Buyer.aggregate([{ $sort: { highestBid: -1 } }, { $limit: 1 }]);
+    const data = await Buyer.find({ user: req.params.id, status: { $ne: "winner" } })
       .populate('user')
       .populate('bidDetail')
       .populate('crop')
-      .populate({ 
-        path: 'createbid', 
-        populate: { 
-          path: 'user_id', 
+      .populate({
+        path: 'createbid',
+        populate: {
+          path: 'user_id',
           model: 'User'
-        } 
+        }
       });
     console.log(data);
-    res.status(200).json({ 
-      topHeightBid : Data[0].highestBid ,
+    res.status(200).json({
+      topHeightBid: Data[0].highestBid,
       details: data,
     });
-  }catch(err){
+  } catch (err) {
     res.status(400).json({
       message: err.message
     })
   }
 }
 
-module.exports.getwinnerByUserId= async(req,res) => {
-  try{
-    const Data = await Buyer.aggregate([{$sort: {highestBid: -1}},{$limit: 1}]);
-    const data = await Buyer.find({user: req.params.id,status:"winner" })
+module.exports.getwinnerByUserId = async (req, res) => {
+  try {
+    const Data = await Buyer.aggregate([{ $sort: { highestBid: -1 } }, { $limit: 1 }]);
+    const data = await Buyer.find({ user: req.params.id, status: "winner" })
       .populate('user')
       .populate('bidDetail')
       .populate('crop')
-      .populate({ 
-        path: 'createbid', 
-        populate: { 
-          path: 'user_id', 
+      .populate({
+        path: 'createbid',
+        populate: {
+          path: 'user_id',
           model: 'User'
-        } 
+        }
       });
     console.log(data);
-    res.status(200).json({ 
-      topHeightBid : Data[0].highestBid ,
+    res.status(200).json({
+      topHeightBid: Data[0].highestBid,
       details: data,
     });
-  }catch(err){
+  } catch (err) {
     res.status(400).json({
       message: err.message
     })
@@ -314,15 +314,15 @@ module.exports.getwinnerByUserId= async(req,res) => {
 //   }
 // }
 
-module.exports.getAcccepted = expressAsyncHandler(async(req, res) => {
-  try{
-    const Data = await User.findById({_id: req.params.id});
-    const data = await Buyer.find({user: req.params.id, status: "Accepted"})
+module.exports.getAcccepted = expressAsyncHandler(async (req, res) => {
+  try {
+    const Data = await User.findById({ _id: req.params.id });
+    const data = await Buyer.find({ user: req.params.id, status: "Accepted" })
     res.status(200).json({
       name: Data.name,
       data: data
     })
-  }catch(err){
+  } catch (err) {
     res.status(400).json({
       message: err.message
     })
@@ -334,25 +334,25 @@ module.exports.getAcccepted = expressAsyncHandler(async(req, res) => {
 module.exports.UpdateBidDetails = expressAsyncHandler(async (req, res) => {
   const id = req.params.id;
   try {
-     await Buyer.updateOne({_id: id}, { 
+    await Buyer.updateOne({ _id: id }, {
       quantity: req.body.quantity,
       product: req.body.product
-     }, {new: true});
+    }, { new: true });
 
-    res.status(200).send({ msg: "Bid is Update ",});
+    res.status(200).send({ msg: "Bid is Update ", });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error, msg: "Error occured" });
   }
 });
 
-exports.DeleteAll = async(req,res) => {
-  try{
+exports.DeleteAll = async (req, res) => {
+  try {
     await Buyer.deleteMany();
     res.status(200).json({
       message: "Deleted "
     })
-  }catch(err){
+  } catch (err) {
     console.log(err)
     res.status(400).json({
       message: err.message
