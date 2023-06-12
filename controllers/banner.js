95c738c2890fce0e2f7e309c967ce842ee6e7c18
rubiyadav <rubiflyeis@gmail.com>
2023-06-12 10:58:48 +0530

    update

--- controllers/banner.js
+++ controllers/banner.js
@@ -1,11 +1,21 @@
 const Banner = require('../models/banner');
-
+const cloudinary = require("cloudinary");
+cloudinary.config({
+  cloud_name: "https-www-pilkhuwahandloom-com",
+  api_key: "886273344769554",
+  api_secret: "BVicyMGE04PrE7vWSorJ5txKmPs",
+});
 
 
 module.exports.addBanner = async (req, res) => {
     try {
+      let photo;
+      if (req.body.image) {
+        var result = await cloudinary.uploader.upload(req.body.image, { resource_type: "auto" });
+        photo = result.secure_url;
+      }
         const addBanner = await Banner.create({
-           image: req.body.image
+           image: photo
         });
         res.status(200).json({
           msg: "Banner successfully added",
